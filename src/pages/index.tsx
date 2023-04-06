7b14381992ffc7e7c071b8058cbed9e06222c6a2
Vitalii <vitaliydps4@gmail.com>
2023-04-06 18:29:14 +0200

    add scss (sass)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        <h1 className={"title-first"}>title</h1>
+      </main>
     </>
   );
 }
