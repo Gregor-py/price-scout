02efb0e218196c704213418bcf3d29112aadcda1
Vitalii <vitaliydps4@gmail.com>
2023-04-12 14:11:21 +0200

    create ModalHeader

--- src/components/modals/AuthModal.tsx
+++ src/components/modals/AuthModal.tsx
@@ -1,5 +1,6 @@
-import { FC, useState } from "react";
+import React, { FC, useState } from "react";
 import { Modal } from "@/components/ui/Modal/Modal";
+import { ModalHeader } from "@/components/ui/Modal/ModalHeader";
 
 export const AuthModal: FC = () => {
   const [showModal, setShowModal] = useState(false);
@@ -14,7 +15,7 @@ export const AuthModal: FC = () => {
         Open regular modal
       </button>
       <Modal showModal={showModal} closeModal={() => setShowModal(false)}>
-        <div className={"text-2xl"}>AuthModal</div>
+        <ModalHeader closeModal={() => setShowModal(false)}>Auth modal</ModalHeader>
         <div>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium ad autem
           delectus dolor fugit inventore iure maiores mollitia natus nisi obcaecati omnis optio quas
--- src/components/ui/Modal/Modal.tsx
+++ src/components/ui/Modal/Modal.tsx
@@ -38,19 +38,7 @@ export const Modal: FC<Modal> = ({
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="bg-gray-700 border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-3 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => closeModal()}
-                  >
-                    <span className="text-white bg-transparent opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                <div className="p-3">{children}</div>
+                {children}
               </div>
             </div>
           </div>
